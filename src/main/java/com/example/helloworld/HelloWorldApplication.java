8b8eaace6a8d89228304e170da04ed5101ae3034
tejaswi1-dev <tejaswipasupula@gmail.com>
2025-08-28 12:36:29 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
+        SpringApplication.run(HelloWorldApplication.class, arg);
     }
 
 }
